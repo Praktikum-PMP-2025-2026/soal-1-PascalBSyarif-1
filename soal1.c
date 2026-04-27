f30c8a4dee4b3af7806b27795f663daddbd5ce24
PascalBSyarif <pascalbsyarif@gmail.com>
2026-04-27 08:55:40 +0700

    soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,135 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main(void) {
-    return 0;
+typedef struct Weather
+{
+    int x;
+    struct Weather *next;
+} Weather ;
+
+Weather *createNode(int x){ 
+    Weather *newNode = (struct Weather*)malloc(sizeof(Weather));
+    newNode->x = x;
+    newNode->next = NULL;
+    return newNode;
+}
+
+Weather *addNode(int x, Weather *head){
+    if (head == NULL)
+    {
+        return createNode(x);
+    }
+    else
+    {
+        Weather *currentNode = head;
+        while (currentNode->next != NULL)
+        {
+            currentNode = currentNode->next;
+        }
+        currentNode->next = createNode(x);
+        return head;
+    }
+}
+
+Weather *sortWeather(Weather *head){ //Mengambil referensi dari geeksforgeeks : https://www.geeksforgeeks.org/dsa/c-program-bubble-sort-linked-list/
+    int swapped, i;
+    struct Weather *ptr1;
+    struct Weather *ptr2 = NULL;
+    do
+    {
+        swapped = 0;
+        ptr1 = head;
+        while (ptr1->next != ptr2)
+        {
+            if (ptr1->x > ptr1->next->x)
+            {
+                int temp = ptr1->x;
+                ptr1->x = ptr1->next->x;
+                ptr1->next->x = temp;
+                swapped = 1;
+            }
+            ptr1 = ptr1->next;
+        }
+        ptr2 = ptr1;
+    } while (swapped);
+    return head;
+}
+
+void *findMedian(Weather *head){
+    int count = 0;
+    float median = 0;
+        Weather *currentNode = head;
+        while (currentNode->next != NULL)
+        {
+            currentNode = currentNode->next;
+            count++;
+        }
+        count++;
+        printf("COUNT %d", count);
+        if (count%2 == 0)
+        {
+            currentNode = head;
+            for (int i = 0; i <= count/2 + 1; i++)
+            {
+                if (i == count/2 - 1)
+                {
+                    median = currentNode->x;
+                }
+                else if (i == count/2)
+                {
+                    median = median + currentNode->x;
+                }
+                currentNode = currentNode->next; 
+            }
+            median = median/2;
+        }
+        else if (count%2 != 0)
+        {
+            currentNode = head;
+            for (int i = 0; i < count/2 - 1; i++)
+            {
+                currentNode = currentNode->next;
+            }
+            median = currentNode->next->x;
+        }
+        currentNode = head;
+        printf("SORTED ");
+        while (currentNode->next != NULL)
+        {
+            printf("%d ", currentNode->x);
+            currentNode = currentNode->next;
+        }
+        printf("%d", currentNode->x);
+        printf("MEDIAN %.2f", median);   
+    }
+
+void *printArray(Weather *head){
+    Weather *currentNode = head;
+    while (currentNode->next != NULL)
+    {
+        printf("%d ", currentNode->x);
+        currentNode = currentNode->next;
+    }
+    printf("%d", currentNode->x);
+    
+}
+
+int main(){
+    int temp = 0;
+    Weather *report = NULL;
+    while (temp != -1)
+    {
+        scanf("%d", &temp);
+        if (temp != -1)
+        {
+            report = addNode(temp, report);
+        }
+        else
+        {
+            break;
+        }
+    }
+    sortWeather(report);
+    findMedian(report);
 }
